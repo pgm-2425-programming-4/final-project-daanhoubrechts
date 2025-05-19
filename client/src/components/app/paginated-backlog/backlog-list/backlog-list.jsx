17a56f4075d8ff2e26852b447e9b97d8cfa1510d
daanhoubrechts <daan.balegem@gmail.com>
2025-05-19 15:36:56 +0200

    feat: update BacklogList component to display items in a table format

--- client/src/components/app/paginated-backlog/backlog-list/backlog-list.jsx
+++ client/src/components/app/paginated-backlog/backlog-list/backlog-list.jsx
@@ -1,9 +1,20 @@
 export function BacklogList({ backlogItems }) {
   return (
-    <ul>
-      {backlogItems.map((backlogItem) => {
-        return <li key={backlogItem.id}>{backlogItem.name}</li>;
-      })}
-    </ul>
+    <table className="table is-hoverable">
+      <thead>
+        <tr>
+          <th>Naam</th>
+        </tr>
+      </thead>
+      <tbody>
+        {backlogItems.map((backlogItem) => {
+          return (
+            <tr key={backlogItem.id}>
+              <td>{backlogItem.name}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
