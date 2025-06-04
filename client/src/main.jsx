be27b1b7a6993fa451fa5d53752ec4bc49c8a463
daanhoubrechts <daan.balegem@gmail.com>
2025-06-04 16:44:34 +0200

    feat: integrate React Query for data fetching in main application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// Import the generated route tree
+
 import { routeTree } from "./routeTree.gen";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Create a new router instance
 const router = createRouter({ routeTree });
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
