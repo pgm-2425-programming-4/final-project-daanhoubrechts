83f1b741121ea46500f1e937f396c2ff4b3fb0a1
daanhoubrechts <daan.balegem@gmail.com>
2025-06-04 16:45:48 +0200

    feat: add project route with data fetching and task display

--- client/src/routes/projects/$projectId.jsx
+++ client/src/routes/projects/$projectId.jsx
@@ -1,14 +1,14 @@
-import { createFileRoute, notFound } from "@tanstack/react-router";
-import { fetchTasksByProject } from "../../queries/fetchTasksByProject";
+import { createFileRoute, notFound, Link } from "@tanstack/react-router";
+import { fetchTasksByProjectId } from "../../queries/fetchTasksByProjectId";
 
-export const Route = createFileRoute("/tasks/$tasksProject")({
+export const Route = createFileRoute("/projects/$projectId")({
   loader: async ({ params }) => {
-    const data = await fetchTasksByProject(params.tasksProject);
+    const data = await fetchTasksByProjectId(params.projectId);
     if (isEmpty(data)) {
-      console.error("No data found for Project:", params.tasksProject);
+      console.error("No data found for Project:", params.projectId);
       throw notFound();
     }
-    console.log("Data fetched for Project:", params.tasksProject, data);
+    console.log("Data fetched for Project:", params.projectId, data);
     return data;
   },
 
@@ -28,6 +28,16 @@ function RouteComponent() {
     <div className="main-content">
       <div className="board">
         <div className="board__column">
+          <div className="mb-4">
+            <Link
+              to="/projects/$projectId/backlog"
+              params={{
+                projectId: Route.useParams().projectId,
+              }}
+            >
+              View Backlog
+            </Link>
+          </div>
           {data.data.map((task) => {
             return (
               <div className="task-card">
