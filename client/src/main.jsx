55d4d5f667d4516c61bf6666d8d44bcd65f0b24c
daanhoubrechts <daan.balegem@gmail.com>
2025-06-11 18:19:32 +0200

    feat: remove unused index.css import from main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 
 import { routeTree } from "./routeTree.gen";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
