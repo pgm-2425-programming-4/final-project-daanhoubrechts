db89839f5ce58ba98edfa2f6d0da48bb348f5fe7
daanhoubrechts <daan.balegem@gmail.com>
2025-06-12 17:30:14 +0200

    implement drag-and-drop functionality for task management in StatusColumn and Task components

--- client/src/components/StatusColumn.jsx
+++ client/src/components/StatusColumn.jsx
@@ -1,6 +1,20 @@
-import { Task } from "./Task";
+import { Task, ItemTypes } from "./Task";
+import { useDrop } from "react-dnd";
+import { useState, useEffect } from "react";
+import { fetchStatuses } from "../queries/statuses/fetchStatuses";
+import { updateTaskStatus } from "../queries/tasks/updateTaskStatus";
 
-export function StatusColumn({ statusName, data, className }) {
+export function StatusColumn({ statusName, data, className, onTaskMoved }) {
+  const [statuses, setStatuses] = useState([]);
+
+  useEffect(() => {
+    const getStatuses = async () => {
+      const fetchedStatuses = await fetchStatuses();
+      setStatuses(fetchedStatuses);
+    };
+    getStatuses();
+  }, []);
+
   const hasValidData = data && data.data && Array.isArray(data.data);
 
   const filteredTasks = hasValidData
@@ -9,8 +23,35 @@ export function StatusColumn({ statusName, data, className }) {
       )
     : [];
 
+  // drop functionaliteit
+  const [{ isOver }, drop] = useDrop(
+    () => ({
+      accept: ItemTypes.TASK,
+      drop: (item) => {
+        const targetStatus = statuses.find(
+          (status) => status.Name === statusName
+        );
+
+        updateTaskStatus(item.documentId, targetStatus.documentId)
+          .then(() => {
+            onTaskMoved();
+          })
+          .catch((error) => {
+            console.error("Error updating task status:", error);
+          });
+      },
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+      }),
+    }),
+    [statuses, statusName, onTaskMoved]
+  );
+
   return (
-    <div className="board__column">
+    <div
+      ref={drop}
+      className={`board__column ${isOver ? "board__column--drag-over" : ""}`}
+    >
       <h2 className={`board__column-header ${className}`}>{statusName}</h2>
       {filteredTasks.map((task) => {
         return <Task key={task.id} task={task} />;
